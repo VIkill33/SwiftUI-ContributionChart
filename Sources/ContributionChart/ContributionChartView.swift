4e7e901009b4cef57386172fa4b88418f26718e0
Vikill Blacks <vikillblacks@VikilldeMacBook-Pro.local>
2023-05-18 20:38:45 +0800

    FIX:
    - UIColor cannot be found on macOS

--- Sources/ContributionChart/ContributionChartView.swift
+++ Sources/ContributionChart/ContributionChartView.swift
@@ -121,9 +121,13 @@ extension Color {
     static let background = Color(NSColor.windowBackgroundColor)
     static let secondaryBackground = Color(NSColor.underPageBackgroundColor)
     static let tertiaryBackground = Color(NSColor.controlBackgroundColor)
-#else
+#endif
+#if os(iOS)
     static let background = Color(UIColor.systemBackground)
     static let secondaryBackground = Color(UIColor.secondarySystemBackground)
     static let tertiaryBackground = Color(UIColor.tertiarySystemBackground)
 #endif
+    #if os(watchOS)
+    static let background = Color.black
+    #endif
 }
